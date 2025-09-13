637090be7d4aa1b62690cce0112c41c1c0ac0b44
Cleberson <clebrsonn@hotmail.com>
2025-09-13 19:47:07 -0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3001',
+        target: 'https://cordeiro360-back.onrender.com',
         changeOrigin: true,
         secure: false,
       },
